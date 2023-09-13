45cc2c045260e06e7d933b5caaa484e05f013149
dianabreik <diana.sam341@gmail.com>
2023-09-13 16:53:40 +0200

    Get_Anfrage + Path Variable

---
+++ src/main/java/com/example/springbootproject/MyController.java
@@ -0,0 +1,22 @@
+package com.example.springbootproject;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/hello")
+public class MyController {
+
+    @GetMapping
+    public String getHello(){
+        System.out.println("Hello, World!");
+        return "Hello, World!";
+    }
+    @GetMapping("/{name}")
+    public String nameEditing(@PathVariable String name){
+        System.out.println("Hello, " + name + "!");
+        return "Hello, " + name + "!";
+    }
+
+  
+
+}
